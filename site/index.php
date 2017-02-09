9f998364ecb9fdad3e3b9a95e24a7a8dd6edafdf
JonasHTV7 <jonas@htv7.net>
2017-02-09 02:09:49 +0100

    Update index.php (#2)
    * Update index.php
    
    * Add files via upload

--- site/index.php
+++ site/index.php
@@ -1,1 +1,37 @@
-#
+<html>
+<head>
+    <title>HTV7pack | Home</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/reset.css">
+</head>
+<body>
+<header>
+    <div class="wrapper">
+        <nav>
+            <ul>
+                <li><a href="https://htv7pack.htv7.net/">Home</a></li>
+                <li><a href="https://htv7pack.htv7.net/download/">Download</a></li>
+                <li><a href="https://htv7pack.htv7.net/about/">About</a></li>
+                <li><a href="https://htv7pack.htv7.net/contact/">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<div class="header-pusher"></div>
+<section>
+    <div class="wrapper">
+        
+    </div>
+</section>
+<footer>
+    <div class="wrapper">
+        <section>
+            <article>
+                <h3></h3>
+                <p><a href=""></a></p>
+            </article>
+        </section>
+    </div>
+</footer>
+</body>
+</html>
