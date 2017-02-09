043260a10c045176b157d405cfb04b8ac196c925
Bent Terp <bent.terp@basefarm.com>
2017-02-09 03:43:16 +0100

    made links relative

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
     <div class="wrapper">
         <nav>
             <ul>
-                <li><a href="https://htv7pack.htv7.net/">Home</a></li>
-                <li><a href="https://htv7pack.htv7.net/download/">Download</a></li>
-                <li><a href="https://htv7pack.htv7.net/about/">About</a></li>
-                <li><a href="https://htv7pack.htv7.net/contact/">Contact</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/download/">Download</a></li>
+                <li><a href="/about/">About</a></li>
+                <li><a href="/contact/">Contact</a></li>
             </ul>
         </nav>
     </div>
